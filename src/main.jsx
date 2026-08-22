a96a72bb10e8845111e6ef112c0ecc78fea08c49
ephraim2005 <35675517+ephraim2005@users.noreply.github.com>
2026-08-22 15:17:13 -0500

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import TaskLens from './TaskLens.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TaskLens />
-  </React.StrictMode>,
-)
